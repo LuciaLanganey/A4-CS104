d1ae0cdbf1aa31b86e2da8094325c41fe57e5bce
LuciaLanganey <langaneylucia03@gmail.com>
2026-05-10 23:07:33 -0700

    Add game logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,5 @@
+import random
+
 # greet user
 print("Hi! Let's play Rock, Paper, Scissors.")
 
@@ -6,3 +8,21 @@ print(
     "Rock beats scissors, scissors beats paper, paper beats rock. "
     "Same choice is a tie."
 )
+
+# game
+choices = ["rock", "paper", "scissors"]
+player = input("Your turn (rock, paper, or scissors): ")
+computer = random.choice(choices)
+
+print("Computer chose:", computer)
+
+if player == computer:
+    print("Tie!")
+elif (
+    (player == "rock" and computer == "scissors")
+    or (player == "scissors" and computer == "paper")
+    or (player == "paper" and computer == "rock")
+):
+    print("You win!")
+else:
+    print("Computer wins!")
